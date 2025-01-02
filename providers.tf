2c42d721772a7d2391f2b104e0c102186517b818
kevinmelkowski <kevin.melkowski@gmail.com>
2025-01-02 11:45:34 -0500

    Adding aws credentials

--- providers.tf
+++ providers.tf
@@ -10,5 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region # Set the AWS region to US East (N. Virginia)
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  region     = var.region # Set the AWS region to US East (N. Virginia)
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
 variable "discord_name" {
   description = "Discord Name"
   type        = string
