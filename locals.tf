0eed1b78a25c1c0abd2ace4e3a553ce9ac44eca2
kevinmelkowski <kevin.melkowski@gmail.com>
2025-01-04 17:50:57 -0500

    Upgrading permissions / hopefully fixing flow

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   account_id      = data.aws_caller_identity.AWS-Details.account_id
   program_name    = "${var.recipient}-${var.discord_name}"
-  recipient_email = "${var.recipient}@${var.discord_name}"
+  recipient_email = "${var.recipient}@${var.domain_name}"
 }
